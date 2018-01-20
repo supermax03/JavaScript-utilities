90b0e8301e6ad9038b59ef7f811e973e34eea233
supermax03 <maximiliano.bordon@gmail.com>
2018-01-19 22:00:43 -0300

    add meliwebscraping

---
+++ webscrapingmeli/meliwebscraping.js
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////
+//Author: Maximiliano Bordon. Testeado para mercado libre argentina
+////////////////////////////////////////////////////////////////////
+
+var Xray=require('x-ray');
+var util=require('util')
+var x=Xray()
+
+getsalesmanprofile=function(salesman) {
+           _url_=util.format('https://perfil.mercadolibre.com.ar/%s',salesman)
+            x(_url_,'.main-wrapper',[{
+               name:'.store-info__name',
+               experience:'.experience',
+               salesamount:'.sales-amount',
+               metrics:['.metric__description h2'],
+               opinion:'.total',    // Este es el total de los votos
+               tags:['.buyers-feedback-qualification'] // Con esto recuperamos el desglose de los votos
+}])
+(console.log)
+}
+getsalesmanprofile('DESILLAS')
